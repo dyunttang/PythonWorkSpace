6645bd1ed0c9115d440d2c9b8cdaa5aed6a00594
ssar <ssar@nate.com>
2021-09-14 11:07:42 +0900

    Python Basic BeautifulSoup started

---
+++ crawproject/html_parse_test.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+
+html_doc = """
+<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title"><b>The Dormouse's story</b></p>
+
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+
+<p class="story">...</p>
+"""
+
+soup = BeautifulSoup(html_doc, 'html.parser')
+
+# print(soup.prettify())
+
+# tag1 = soup.p
+
+# print(tag1)
+
+# tag2 = soup.a
+
+# print(tag2)
+
+# print(tag2["href"])
+
+# print(tag2.attrs)
+
+tag3 = soup.find(class_="title")
+
+print(tag3)
--- crawproject/news_title.py
+++ crawproject/news_title.py
@@ -1,13 +1,16 @@
 import requests  # 통신하기 위해 제일 먼제 해야할 것!
 
+# Crawling 하기 위해 해야할 것: 1.주소분석, 2.알고리즘(for문 돌면서 다운받는 것), 3.text/html 이기 때문에 parsing 하는 게 힘듦
+
 # https://newsstand.naver.com/?list=&pcode=001 주소를 보니 url 이 아니라 uri 이다.
 # 주소를 보니 source 를 request 하는 것이 아니라 identifier 이다
 # 신문사 이름을 crawling
 # 목적 : oid 수집
 start_oid = 1
 #oid_list = []
+oid_names = []
 
-for num in range(0, 1000):
+for num in range(0, 2):
     start_oid_str = str(start_oid).zfill(3)
     uri = f"https://newsstand.naver.com/?list=&pcode={start_oid_str}"
 
@@ -19,11 +22,12 @@ for num in range(0, 1000):
 
     if(response.status_code == 200):
         # pcode 를 담아야 하는데, 이것을 담으려면 variable 로 만들어야 한다 -> start_oid = "001"
-        oid_list.append(start_oid_str)
+        # oid_list.append(start_oid_str)
+        print(response.text)
 
     start_oid = start_oid + 1
 
-    print(f"oid 총 개수 : {len(oid_list)}")
-    print(oid_list)
+    # print(f"oid 총 개수 : {len(oid_list)}")
+    # print(oid_list)
 
 # 001, 002, 003 ... 이것은 숫자가 아니라 문자인데 어떻게 하면 +1씩 증가할 수 있을까? oid_test.py 만들어서 test
